bdef4dd18ffa325f6e61ba061b3618fcde14e809
Bola-Kadiri <bolajimoh25@gmail.com>
2023-07-17 16:22:24 -0700

    faq page css

--- src/pages/Faq.js
+++ src/pages/Faq.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import "../styles/Faq.css"
 import { CiSearch } from 'react-icons/ci';
+import down from "../asset/down.png"
+import finder from "../asset/finder.png"
+import { Link } from 'react-router-dom';
 
 const Faq = () => {
   return (
@@ -11,7 +14,7 @@ const Faq = () => {
          <div className='bg-faq-overlay'>
               <div className='faq-container'>
                    <div className='faq-content'>
-                       <h1>How can we help you</h1>
+                       <h1>How can we help you?</h1>
                         <div className='search-box'>
                              <CiSearch style={{fontSize: "30px"}}/>
                              <input type="search" placeholder='Search for answers'/>
@@ -74,7 +77,53 @@ const Faq = () => {
     </div>
     <div className='question'>
          <div className='question-content'>
-              <div></div>
+              <h3>Frequently asked Questions</h3>
+              <div className='question-section'>
+                   <div className='question-card'>
+                        <p>Do you Over Customer Support?</p>
+                        <img src={down} alt={down}/>
+                   </div>
+                   <div className='question-card'>
+                        <p>Do you Over Customer Support?</p>
+                        <img src={down} alt={down}/>
+                   </div>
+                   <div className='question-card'>
+                        <p>Do you Over Customer Support?</p>
+                        <img src={down} alt={down}/>
+                   </div>
+                   <div className='question-card'>
+                        <p>Do you Over Customer Support?</p>
+                        <img src={down} alt={down}/>
+                   </div>
+                   <div className='question-card'>
+                        <p>Do you Over Customer Support?</p>
+                        <img src={down} alt={down}/>
+                   </div>
+                   <div className='question-card'>
+                        <p>Do you Over Customer Support?</p>
+                        <img src={down} alt={down}/>
+                   </div>
+                   <div className='question-card'>
+                        <p>Do you Over Customer Support?</p>
+                        <img src={down} alt={down}/>
+                   </div>
+                   <div className='question-card'>
+                        <p>Do you Over Customer Support?</p>
+                        <img src={down} alt={down}/>
+                   </div>
+                   <div className='question-card'>
+                        <p>Do you Over Customer Support?</p>
+                        <img src={down} alt={down}/>
+                   </div>
+              </div>
+         </div>
+    </div>
+    <div className='find-section'>
+         <div className='find-section-content'>
+              <h3>Didn’t find what you are looking for?</h3>
+              <p>Our Team is active to attend to your email</p>
+              <img src={finder} alt={finder}/>
+              <Link to="/contact"><button>Contact Us</button></Link>
          </div>
     </div>
     </React.Fragment>
